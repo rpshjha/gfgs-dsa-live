8cba8ff94d682ffc75a47abb1ee172d74bd2afd2
Rupesh Kumar <rupeshkumar@192.168.1.6>
2022-12-04 15:52:15 +0530

    day2

--- gfsk-dsa-java-project/src/day2/WavePrint.java
+++ gfsk-dsa-java-project/src/day2/WavePrint.java
@@ -1,30 +1,38 @@
 package day2;
 
+import java.util.Scanner;
+
 public class WavePrint {
 
 
     public static void main(String[] args) {
 
-        int[][] arr = new int[3][3];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter row");
+        int row = sc.nextInt();
+        System.out.println("enter col");
+        int col = sc.nextInt();
 
-        arr[0][0] = 1;
-        arr[0][1] = 2;
-        arr[0][2] = 3;
-        arr[1][0] = 4;
-        arr[1][1] = 5;
-        arr[1][2] = 6;
-        arr[2][0] = 7;
-        arr[2][1] = 8;
-        arr[2][2] = 9;
+        System.out.println("enter data");
+        int[][] arr = new int[row][col];
 
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
 
+        printWave(arr);
+    }
+
+    private static void printWave(int[][] arr) {
         for (int row = 0; row < arr.length; row++) {
 
             if (row % 2 == 0)
                 for (int col = 0; col < arr.length; col++)
                     System.out.print(arr[row][col] + " ");
             else
-                for (int col = arr.length -1; col >= 0; col--)
+                for (int col = arr.length - 1; col >= 0; col--)
                     System.out.print(arr[row][col] + " ");
 
             System.out.println();
